2d9e5287d85cd3c7decdad05fafc99115f166ad5
5soros <87423801+5soros@users.noreply.github.com>
1995-10-24 18:48:04 +0800

    1995

--- 自动填下期预测数据的模版/主页用/auto-renew/vv.js
+++
@@ -1,9 +0,0 @@
-const xiaoHua = require('./xiaohua.js');
-
-function getRandoms(length) {
-    return Math.floor(Math.random() * length);
-}
-
-for(let i = 0; i <100; i++){
-    console.log(xiaoHua[getRandoms(xiaoHua.length)]);
-}
